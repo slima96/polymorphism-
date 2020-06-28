25a6f22d8aab4950da66788e2d64a35a7f960336
Alexsandro Leão Lima <sandro.lima@viatek.local>
2020-06-28 20:00:16 -0300

    Modificações na classe principal e criação de uma lista do tipo Product

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,24 @@
 package application;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
 public class Program {
 
 	public static void main(String[] args) {
-
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		List<Product> list = new ArrayList<>();
+		
+		
 	}
 
 }
