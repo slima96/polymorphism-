2e55f6256dafebcc21095f9a2db89a63c7463bc3
Alexsandro Leão Lima <sandro.lima@viatek.local>
2020-06-28 19:30:55 -0300

    Construtor com argumentos, modificação no construtor padrão e modificação na declaração da classe produtos Importados

--- src/entities/ImportedProduct.java
+++ src/entities/ImportedProduct.java
@@ -1,10 +1,16 @@
 package entities;
 
-public class ImportedProduct {
+public class ImportedProduct extends Product {
 	private Double customsFee;
 	
 	public ImportedProduct() {
+		super();
 		
 	}
 
+	public ImportedProduct(String name, Double price, Double customsFee) {
+		super(name, price);
+		this.customsFee = customsFee;
+	}
+		
 }
