6f4982e928890154b0bb0d9105c2f70a555730cc
Alexsandro Leão Lima <sandro.lima@viatek.local>
2020-06-28 19:46:48 -0300

    Construtor com atributos criado, modificação na declaração da classe ProdutosUsados, modificação no construtor padrão

--- src/entities/UsedProduct.java
+++ src/entities/UsedProduct.java
@@ -2,11 +2,17 @@ package entities;
 
 import java.util.Date;
 
-public class UsedProduct {
+public class UsedProduct extends Product {
 	private Date manufactureDate;
 	
 	public UsedProduct() {
-		
+		super();
 	}
 
+	public UsedProduct(String name, Double price, Date manufactureDate) {
+		super(name, price);
+		this.manufactureDate = manufactureDate;
+	}
+	
+	
 }
