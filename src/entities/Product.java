246cda241438c464fa4eb7857c0c8822a9e1ddf7
Alexsandro Leão Lima <sandro.lima@viatek.local>
2020-06-28 19:02:46 -0300

    gerado metodos getters and setters

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -14,4 +14,21 @@ public class Product {
 		this.price = price;
 	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	
 }
