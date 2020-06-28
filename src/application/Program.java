951aae5fa7108171ea455b5392eb10a78cd41ce5
Alexsandro Leão Lima <sandro.lima@viatek.local>
2020-06-28 20:12:25 -0300

    Condições para verifica o tipo do produto e adicionar cada produtos conforme o seu tipo numa lista

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,20 @@
 package application;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.ImportedProduct;
 import entities.Product;
+import entities.UsedProduct;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
@@ -31,7 +35,24 @@ public class Program {
 			System.out.print("Price: ");
 			double price = sc.nextDouble();
 			
+			if (type =='i') {
+				System.out.print("Custom fee: ");
+				double customsFee = sc.nextDouble();
+				
+				list.add(new ImportedProduct(name, price, customsFee));
+			}
+			if ( type =='c') {
+				list.add(new Product(name, price));
+			}
+			if (type =='u') {
+				System.out.print("Manufacture date (DD/MM/YYYY): ");
+				Date manufactureDate = sdf.parse(sc.next());
+				
+				list.add(new UsedProduct(name, price, manufactureDate));
+			}
 		}
+		
+		
 	}
 
 }
